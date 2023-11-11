408cba485287a843bd8590c28602b127174664a0
abbas usman <johnmccain2244@gmail.com>
2023-11-11 18:30:56 +0900

    feat: checking react routing doc

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import '../css/App.css';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { RippleBadge } from './MaterialTheme/styled';
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import React from "react";
+import "../css/App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { RestaurantPage } from "./screens/RestaurantPage";
+import { CommunityPage } from "./screens/CommunityPage";
+import { OrdersPage } from "./screens/OrdersPage";
+import { MembersPage } from "./screens/MembersPage";
+import { HelpPage } from "./screens/HelpPage";
+import { LoginPage } from "./screens/LoginPage";
+import { HomePage } from "./screens/HomePage";
 
 function App() {
-  return (
-    <Container maxWidth="sm">
-      <Stack flexDirection={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h1"} gutterBottom>
-            Create React App on TypeScript with Redux
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-          <Button color="secondary" variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    return (
+        // <div>
+        //     <Users />
+        //     <Dishes />
+        // </div>
 
-  );
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/restaurant">RestaurantPage</Link>
+                        </li>
+                        <li>
+                            <Link to="/community">CommunityPage</Link>
+                        </li>
+                        <li>
+                            <Link to="/orders">OrdersPage</Link>
+                        </li>
+                        <li>
+                            <Link to="/members">MembersPage</Link>
+                        </li>
+                        <li>
+                            <Link to="/help">HelpPage</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">LoginPage</Link>
+                        </li>
+                        <li>
+                            <Link to="/">HomePage</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Switch>
+                    <Route path="/restaurant">
+                        <RestaurantPage />
+                    </Route>
+                    <Route path="/community">
+                        <CommunityPage />
+                    </Route>
+                    <Route path="/orders">
+                        <OrdersPage />
+                    </Route>
+                    <Route path="/members">
+                        <MembersPage />
+                    </Route>
+                    <Route path="/help">
+                        <HelpPage />
+                    </Route>
+                    <Route path="/login">
+                        <LoginPage />
+                    </Route>
+                    <Route path="/">
+                        <HomePage />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
+
+function Home() {
+    return <h2>Home</h2>;
+}
