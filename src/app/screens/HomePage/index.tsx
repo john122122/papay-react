00ab09875c19c5a0e48090d76a72c29dc69a803e
abbas usman <johnmccain2244@gmail.com>
2023-12-23 10:27:48 +0900

    fix: usage of slice and selector on homePage screen component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -1,28 +1,49 @@
 import React, { useEffect } from "react";
+import { Container } from "@mui/material";
 import { Statistics } from "./statistics"
 import { TopRestaurants } from "./topRestaurants";
 import { BestRestaurants } from "./bestRestaurants";
 import { BestDishes } from "./bestDishes";
 import { Advertisements } from "./advertisements";
 import { Events } from "./events";
-import '../../../css/home.css';
 import { Recomendations } from "./recommendations";
+import '../../../css/home.css';
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+import { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setTopRestaurants } from "../../screens/HomePage/slice";
+import { 
+  retrieveTopRestaurants, 
+} from "../../screens/HomePage/selector";
+import { Restaurant } from "../../../types/user";
+
+/** Redux slice */ 
+const actionDispatch = (dispatch: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispatch(setTopRestaurants(data)),
+});
+
+/** Redux selector */
+const topRestaurantRetriever = createSelector (
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
 
 export function HomePage() {
+  /** Initialisation */
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(topRestaurantRetriever);
 
-  // selector store => data
+  console.log("topRestaurants::", topRestaurants);
 
   useEffect(() => {
     // backend data request => data
-
-    // slice: data => store
-    // console.log("componentDidMount => Data fetch");
-
-    return () => {
-      // console.log("componentWillUnmount process")
-    }
-
+  setTopRestaurants([]);
   }, []);
+  
     return <div className="homepage">
       <Statistics/>
       <TopRestaurants/>
